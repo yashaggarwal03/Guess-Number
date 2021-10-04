38c34645009a37d11861114b253215ea89f27a8d
Yash <yash.agg2000@gmail.com>
2021-10-05 00:06:16 +0530

    Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
+import Colors from '../constants/Colors'
+
 const Header = props => {
   return (
     <View style={styles.header}>
@@ -14,7 +16,7 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 90,
     // paddingTop: 20,
-    backgroundColor: '#f7287b',
+    backgroundColor: Colors.primary,
     justifyContent: 'center',
     alignItems: 'center',
   },
