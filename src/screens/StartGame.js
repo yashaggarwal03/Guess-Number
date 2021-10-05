fd40af766650ee27dca23c9dc00b5bfca4a1e685
Yash <yash.agg2000@gmail.com>
2021-10-05 12:32:02 +0530

    Start Game Screen

--- src/screens/StartGame.js
+++ src/screens/StartGame.js
@@ -1,11 +1,57 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
+
+import Card from '../components/Card';
+import Input from '../components/Input';
+import Colors from '../constants/Colors';
 
 const StartGame = () => {
+  const [number, setNumber] = useState('');
+
+  const EnteredNumber = number => {
+    setNumber(number.replace(/[^0-9]/g, ''));
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Start the Game!</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Start a New Game!</Text>
+        <Card style={styles.inputContainer}>
+          <Text style={{color: '#000'}}>Select a Number</Text>
+          <Input
+            style={styles.input}
+            keyboardType="number-pad"
+            maxLength={2}
+            allowFontScaling={false}
+            onChangeText={EnteredNumber}
+            value={number}
+          />
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+              <Button
+                title="Reset"
+                onPress={() => {}}
+                color={Colors.secondary}
+              />
+            </View>
+            <View style={styles.button}>
+              <Button
+                title="Confirm"
+                onPress={() => {}}
+                color={Colors.primary}
+              />
+            </View>
+          </View>
+        </Card>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -14,6 +60,29 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    color: '#000',
+    marginVertical: 10,
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: '80%',
+    alignItems: 'center',
+  },
+  input: {
+    width: 50,
+    textAlign: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15,
+  },
+  button: {
+    width: 100,
   },
 });
 
