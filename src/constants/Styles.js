9f83bc06a3505a9433de1643fedb808e6887a670
Yash <yash.agg2000@gmail.com>
2021-10-07 22:47:49 +0530

    Custom Styles

---
+++ src/constants/Styles.js
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  title: {
+    fontSize: 20,
+    color: '#000',
+    marginVertical: 10,
+    fontFamily: 'OpenSans-Bold',
+  },
+  text: {
+    fontFamily: 'OpenSans-Regular',
+    color: '#000',
+  },
+  largeText: {
+    fontFamily: 'OpenSans-Regular',
+    fontSize: 18,
+    color: '#000',
+    textAlign: 'center',
+  },
+});
